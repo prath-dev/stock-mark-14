c9d06f4fe203c7a5b14a2a347271b582d74ad971
prath-dev <73606133+prath-dev@users.noreply.github.com>
2021-10-07 18:56:37 +0530

    bugs
    negative value bugs fixed

--- index.js
+++ index.js
@@ -13,20 +13,18 @@ function clickHandeler () {
      let cp = Number(currentprice.value );   
      calculateprofitloss(ip,qty,cp) ;
 }
-        function calculateprofitloss (initial,quantity,current) {
-            if (initial > current) {   
+        function calculateprofitloss (initial,quantity,current) { 
+         if(initial<1 || current<1){
+            output.innerText="Initial Price/Current price cannot be 0 or in negatives"
+            document.body.style.backgroundColor="blue" ;
+        }
+          else if (initial > current) {   
                 let loss = ((  initial - current) * quantity).toFixed(2) ;
              let Losspercentage = ( (loss /initial) * 100).toFixed(2);   
              output.innerText= "the loss is" + loss + "and the loss percentage is" + Losspercentage ; 
              document.body.style.backgroundColor = "red"
                
  } 
- else if    (current> initial ) {
-                 let profit = ((  current - initial) * quantity).toFixed(2) ; 
-                 let profitpercentage = ( (profit * initial) * 100).toFixed(2) ; 
-                 output.innerText= "the profit is" + profit + "and the profit percentage is" + profitpercentage ; 
-                 document.body.style.backgroundColor = "green" 
-                    } 
              
  else if    (current> initial ) {
     let profit = ((  current - initial) * quantity).toFixed(2) ; 
@@ -34,12 +32,6 @@ function clickHandeler () {
     output.innerText= "the profit is" + profit + "and the profit percentage is" + profitpercentage ; 
     document.body.style.backgroundColor = "green" 
        } 
-
-else if (initial=="" || quantity=="" ||current==""|| initial<0 || quantity<0 ||current<0) 
-{
-output.innerText = "input should not be empty, negative and 0 ";
-document.body.style.backgroundColor = "yellow"  ;
-}   
 else {
    output.innerText= "No loss NO profit" ; 
    document.body.style.backgroundColor = "blue"  ;
